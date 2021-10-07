ad3408a761e2a738555e55b39b9acff31195a4f5
hiroko-i <90242962+hiroko-i@users.noreply.github.com>
2021-10-07 03:25:57 +0000

    [Update]fontawesome修正

--- app/controllers/blogs_controller.rb
+++ app/controllers/blogs_controller.rb
@@ -6,6 +6,8 @@ class BlogsController < ApplicationController
 
   def index
     @blogs = Blog.all
+    p "標準出力のみに反映"
+    logger.debug("標準出力とログファイルに記録される")
   end
 
   def new
@@ -24,13 +26,13 @@ class BlogsController < ApplicationController
   def edit
     @blog = Blog.find(params[:id])
   end
-  
+
   def update
     blog = Blog.find(params[:id])
     blog.update(blog_params)
     redirect_to blog_path(blog)
   end
-  
+
   def destroy
     blog = Blog.find(params[:id])
     blog.destroy
